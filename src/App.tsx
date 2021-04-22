319a3ac9a2671672cc9f0a5b4cb357690bc0f61a
Krymeq <krymeq353@gmail.com>
2021-04-22 19:27:37 +0200

    Removed Create-React-App boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Redirect, Switch } from 'react-router-dom';
+import { SearchPage } from './pages/search/search_page';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <SearchPage/>
+          </Route>
+          <Route path="/">
+            <Redirect to="/search"/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/search/search_page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./search_page.scss";
+
+export const SearchPage = () => {
+    return (
+        <div className="search-page-root">
+            <div className="search-panel">
+                dawd
+            </div>
+        </div>
+    )
+}
